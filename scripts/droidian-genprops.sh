d06ed21f92730e95ba76e9e461bfcfdecaaaf24e
r3vn <giuseppe.corti@protonmail.com>
2021-08-07 01:27:10 +0200

    [droidian-genprops] FIXME: hardcoded some props for now

--- scripts/droidian-genprops.sh
+++ scripts/droidian-genprops.sh
@@ -16,9 +16,13 @@ echo "${EXT_LIB_PROP}" >> ${WAYDROID_PATH}/anbox.prop
 echo "${VULKAN_PROP}" >> ${WAYDROID_PATH}/anbox.prop
 echo "${DPI_PROP}" >> ${WAYDROID_PATH}/anbox.prop
 echo "${GLES_VER_PROP}" >> ${WAYDROID_PATH}/anbox.prop
-echo "${XDG_PROP}" >> ${WAYDROID_PATH}/anbox.prop
-echo "${WAYLAND_DISP_PROP}" >> ${WAYDROID_PATH}/anbox.prop
-echo "${PULSE_PROP}" >> ${WAYDROID_PATH}/anbox.prop
+
+# FIXME
+echo "anbox.xdg_runtime_dir=/run/user/32011" >> ${WAYDROID_PATH}/anbox.prop
+echo "anbox.wayland_display=wayland-0" >> ${WAYDROID_PATH}/anbox.prop
+echo "anbox.pulse_runtime_path=/run/user/32011/pulse" >> ${WAYDROID_PATH}/anbox.prop
+echo "anbox.display_height_padding=200" >> ${WAYDROID_PATH}/anbox.prop
 
 # TODO: Drop this
 echo "anbox.active_apps=full" >> ${WAYDROID_PATH}/anbox.prop
+
